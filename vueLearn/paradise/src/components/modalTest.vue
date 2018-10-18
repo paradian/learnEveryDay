5909906f549692dee7fc149a8b09569bb27867c1
Nightaster <1063229235@qq.com>
2018-10-18 20:48:14 +0800

    nothing

---
+++ vueLearn/paradise/src/components/modalTest.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="modal" @click="closeModal" v-if="modalState">
+        <div class="item">
+            <h1>hello world</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+name:"modalTest",
+  data() {
+    return {
+      test: "你好",
+      modalState: false
+    };
+  },
+  props: ["modalState"],
+  methods: {
+    closeModal() {
+      this.$emit("event", false);
+    }
+  }
+};
+</script>
